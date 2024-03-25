8da6e8f95b252446c55ff45d27fa4eaee6159509
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2024-03-25 14:28:51 +0530

    rearranged order of pages

---
+++ frontend/pages/2_Queue.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Job Queue")
+st.write("To view full worker dashboard, click [here](http://localhost:5555/)")
+st.components.v1.iframe("http://localhost:5555/tasks", height=800)
--- frontend/pages/Queue.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Job Queue")
-st.write("To view job tracker, visit celery flower dashboard at http://localhost:5555/tasks")
